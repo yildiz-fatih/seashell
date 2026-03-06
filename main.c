445360cf7098faf1ae9373fc27f197d2c1b4afb7
Fatih <122681611+yildiz-fatih@users.noreply.github.com>
2026-03-06 22:30:41 +0300

    add REPL loop and read user input

--- main.c
+++ main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char *read_line();
 
 int main(void) {
+  while (1) {
     printf("$ ");
-    return 0;
+
+    char *line = read_line();
+    if (line == NULL) {
+      break;
+    }
+
+    // for testing
+    printf("%s\n", line);
+
+    free(line);
+  }
+  return 0;
+}
+
+char *read_line() {
+  char *line = NULL;   // dynamically allocated buffer
+  size_t capacity = 0; // current buffer capacity
+
+  ssize_t len = getline(&line, &capacity, stdin); // excludes '\0'
+  // Check for error or EOF
+  if (len == -1) {
+    free(line);
+    return NULL;
+  }
+
+  // Remove trailing newline
+  if (len > 0 && line[len - 1] == '\n') {
+    line[len - 1] = '\0';
+  }
+
+  return line;
 }
